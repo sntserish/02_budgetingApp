6c44de8ca5a9e3f9ecb14119d5b781569281b497
erishreysantos <santoserishrey@gmail.com>
2024-12-21 20:50:56 +0800

    init commit

---
+++ src/test/java/es/projects/__budgetingApp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.projects.__budgetingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
